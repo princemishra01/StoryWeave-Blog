5d653cdb50afe118d33a12f6e4bd6ed9fa09ecb0
Prince <prince.pm.mishra@gmail.com>
2023-10-28 04:24:27 +0530

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import {Container, Logo, LogoutBtn} from '../index'
+import { Link } from 'react-router-dom'
+import {useSelector} from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+function Header() {
+  const authStatus = useSelector((state) => state.auth.status)
+  const navigate = useNavigate()
+
+  const navItems = [
+    {
+      name: 'Home',
+      slug: "/",
+      active: true
+    }, 
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+  },
+  {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+  },
+  {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+  },
+  {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+  },
+  ]
+
+
+  return (
+    <header className='py-3 shadow bg-[#D2E3C8]-500'>
+      <Container>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link to='/'>
+              <Logo />
+
+              </Link>
+          </div>
+          <ul className='flex ml-auto'>
+            {navItems.map((item) => 
+            item.active ? (
+              <li key={item.name} className='flex text-center'>
+                <button
+                onClick={() => navigate(item.slug)}
+                className='inline-bock px-4 m-2 duration-200 hover:bg-[#86A789] rounded-full'
+                >{item.name}</button>
+              </li>
+            ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+        </Container>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div className='my-auto text-[2.2rem] text-[#86A789] font-black'>StoryWeave</div>
+  )
+}
+
+export default Logo
